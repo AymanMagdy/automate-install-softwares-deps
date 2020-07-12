3aaf970e66291fea5e4b653a88cc716546d095e2
AymanMagdy <magdia883@gmail.com>
2020-07-12 20:11:45 +0200

    Editing the readme file

---
+++ README.md
@@ -0,0 +1,23 @@
+# Automation installation
+
+Automation installation is to automate installing different softwares like Java, Jenkins, virtualBox, etc...
+
+## Installation
+
+git clone https://github.com/AymanMagdy/automate-install-softwares-deps.git
+
+
+## Usage
+
+
+```bash
+cd automate-install-softwares-deps \
+sudo bash automation_software_installtion.sh
+```
+
+## Contributing
+Pull requests with new functions are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- automation_software_installtion.sh
+++ automation_software_installtion.sh
@@ -162,3 +162,21 @@ install_kubectl () {
     # To ensure the user the kubectl is installed
     kubectl version -o json
 }
+
+# Install python3.8
+install_python () {
+    local _python_version=$1
+    _log "Check up" "Python version" $_python_version
+    printf "Updating the system...\n"
+    sudo apt update
+    printf "Installing deps..\n"
+    sudo apt install software-properties-common
+    printf "Adding the repo to the machine..\n"
+    echo -ne '\n' | sudo add-apt-repository ppa:deadsnakes/ppa
+    printf "Installing python3.8.."
+    sudo apt install python3.8
+    python3.8 --version
+}
+
+
+install_python "3.8.0"
