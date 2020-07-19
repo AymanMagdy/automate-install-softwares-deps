4a6def99c5be76616354d8190833fdbd2c0a69ea
SOLIMAN Ayman <Ayman.SOLIMAN@cegedim.com>
2020-07-19 16:45:01 +0200

    Changing checking java

--- programming languges/install_java.sh
+++ programming languges/install_java.sh
@@ -11,21 +11,22 @@ install_java () {
     source ./../sys\ out/warn.sh
     _log "Sourced the needed files, securely.."
 
-    # Checking if Java installed on the machine.
     java --version >& /dev/null
-    if [ $? -ge 1 ]; then
+    _is_java_installed=$?
+    
+    if [ $_is_java_installed -ge 1 ]; then
         _log "Installing" "Java"
         _log "Installing" "JDK" "11.0.1"
-        cd /home/$USER_NAME; \
+        cd /home/$USERNAME; \
         curl -L -C - -b "oraclelicense=accept-securebackup-cookie" \
         -O http://download.oracle.com/otn-pub/java/jdk/11.0.1+13/90cf5d8f270a4347a95050320eef3fb7/jdk-11.0.1_linux-x64_bin.tar.gz
 
         _log "Installed" "JDK" "11.0.1"
         _log "Configuring" "JAVA_HOME..."
 
-        _installed_java=$(ls /home/$USER_NAME | grep ^jdk-[0-9]*.*.tar.gz$)
+        _installed_java=$(ls /home/$USERNAME | grep ^jdk-[0-9]*.*.tar.gz$)
         sudo mkdir -p /opt/jdk; \ 
-             cp -rf /home/$USER_NAME/$_installed_java /opt/jdk; \
+             cp -rf /home/$USERNAME/$_installed_java /opt/jdk; \
              tar -zxf /opt/jdk/$_installed_java
         _untared_jdk=$(ls | grep ^jdk-[0-9]*.[0-9].[0-9]$)
         sudo update-alternatives --install /usr/bin/java java /opt/jdk/$_untared_jdk/bin/java 100; \
